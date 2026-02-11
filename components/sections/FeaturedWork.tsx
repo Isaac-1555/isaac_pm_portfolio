d753fceaea5e6640b7e37b091ba36057503a6be6
Isaac Daniel <benisaac1892@gmail.com>
2026-02-11 14:22:19 -0700

    UI Fixes

--- components/sections/FeaturedWork.tsx
+++ components/sections/FeaturedWork.tsx
@@ -65,7 +65,7 @@ export function FeaturedWork() {
         </div>
 
         {/* Cards Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
           {projects.map((project) => {
             const Icon = project.icon;
             return (
